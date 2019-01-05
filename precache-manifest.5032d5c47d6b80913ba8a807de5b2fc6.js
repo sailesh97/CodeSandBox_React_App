f4a06ca935e07303735793068e2e265ab9f0aea5
sailesh97 <33670045+sailesh97@users.noreply.github.com>
2019-01-05 22:44:56 +0530

    Updates

---
+++ precache-manifest.5032d5c47d6b80913ba8a807de5b2fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c8037eedfbd4b93969e88b7c7b06a35",
+    "url": "/Codesandbox_React_App/static/media/navimg.2c8037ee.png"
+  },
+  {
+    "revision": "1af345f09e666e410286",
+    "url": "/Codesandbox_React_App/static/js/runtime~main.1af345f0.js"
+  },
+  {
+    "revision": "5447f2ad3c19c0efb64b",
+    "url": "/Codesandbox_React_App/static/js/main.5447f2ad.chunk.js"
+  },
+  {
+    "revision": "6e711399c9f907e09093",
+    "url": "/Codesandbox_React_App/static/js/1.6e711399.chunk.js"
+  },
+  {
+    "revision": "5447f2ad3c19c0efb64b",
+    "url": "/Codesandbox_React_App/static/css/main.ec056d22.chunk.css"
+  },
+  {
+    "revision": "cbde3803f25984cfe3d318576b33355e",
+    "url": "/Codesandbox_React_App/index.html"
+  }
+];
